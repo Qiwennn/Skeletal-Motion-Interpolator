2c33cb867516183986362a356f43390c799a6542
Qiwen Zhu <167449913+Qiwennn@users.noreply.github.com>
2026-03-03 14:53:45 -0800

    Create motion.h

---
+++ motion.h
@@ -0,0 +1,56 @@
+#ifndef _MOTION_H_
+#define _MOTION_H_
+
+#include "vector.h"
+#include "types.h"
+#include "posture.h"
+#include "skeleton.h"
+
+class Motion 
+{
+  //function members
+public:
+
+  // parse AMC file (default scale=0.06)
+  Motion(char *amc_filename, double scale, Skeleton * pSkeleton);
+
+  //Use to create default motion with specified number of frames
+  Motion(int numFrames, Skeleton * pSkeleton);
+
+  ~Motion();
+
+  // scale is a parameter to adjust the translationalal scaling
+  // the value of scale should be consistent with the scale parameter used in Skeleton()
+  // forceAllJointsBe3DOF should be set to 0; use 1 to signal that the file contains three Euler
+  // angles for all the joints, even those that are 1-dimensional or 2-dimensional (advanced usage)
+  int writeAMCfile(char* filename, double scale, int forceAllJointsBe3DOF=0);
+
+  //Set all postures to default posture
+  //Root position at (0,0,0), orientation of each bone to (0,0,0)
+  void SetPosturesToDefault();
+
+  //Set the entire posture at specified frame (posture = root position and all bone rotations)
+  void SetPosture(int frameIndex, Posture InPosture);
+
+  //Set root position at specified frame
+  void SetRootPos(int frameIndex, vector vPos);
+  //Set specified bone rotation at specified frame
+  void SetBoneRotation(int frameIndex, int boneIndex, vector vRot);
+
+  int GetNumFrames() { return m_NumFrames; }
+  Posture * GetPosture(int frameIndex);
+
+  Skeleton * GetSkeleton() { return pSkeleton; }
+
+protected:
+  int m_NumFrames; //number of frames in the motion 
+  Skeleton * pSkeleton;
+  //Root position and all bone rotation angles for each frame (as read from AMC file)
+  Posture * m_pPostures; 
+
+  // The default value is 0.06
+  int readAMCfile(char* name, double scale);
+};
+
+#endif
+
